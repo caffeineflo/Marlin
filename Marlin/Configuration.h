3dcc2cb6b72f9de65f2348c92056e65c5f725e82
Florian Harr <harr.florian@gmx.de>
2016-06-14 01:26:15 +0200

    hot bed autotune PID values

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -296,9 +296,9 @@
 
   //120V 250W silicone heater into 4mm borosilicate (MendelMax 1.5+)
   //from FOPDT model - kp=.39 Tp=405 Tdead=66, Tc set to 79.2, aggressive factor of .15 (vs .1, 1, 10)
-  #define  DEFAULT_bedKp 10.00
-  #define  DEFAULT_bedKi .023
-  #define  DEFAULT_bedKd 305.4
+  #define  DEFAULT_bedKp 170.005
+  #define  DEFAULT_bedKi 31.45
+  #define  DEFAULT_bedKd 229.86
 
   //120V 250W silicone heater into 4mm borosilicate (MendelMax 1.5+)
   //from pidautotune
