b6a801da5db4fb7fc5fa8fa3ff2d6f999fed13d8
Florian Harr <harr.florian@gmx.de>
2017-09-04 20:36:21 -0400

    well...

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -772,15 +772,15 @@ const bool Z_MIN_PROBE_ENDSTOP_INVERTING = true; // set to true to invert the lo
 // @section machine
 
 // The size of the print bed
-#define X_BED_SIZE 220
-#define Y_BED_SIZE 220
+#define X_BED_SIZE 205
+#define Y_BED_SIZE 205
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
 #define X_MIN_POS 0
 #define Y_MIN_POS 0
 #define Z_MIN_POS 0
-#define X_MAX_POS 200
-#define Y_MAX_POS 200
+#define X_MAX_POS X_BED_SIZE
+#define Y_MAX_POS Y_BED_SIZE
 #define Z_MAX_POS 260
 
 // If enabled, axes won't move below MIN_POS in response to movement commands.
